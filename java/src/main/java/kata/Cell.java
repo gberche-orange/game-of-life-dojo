03c36c0e87c07a25088c832b28abbaf9595bacfc
Guillaume Berche <guillaume.berche@orange.com>
2014-04-01 12:49:50 +0200

    alive or dead neighbors

--- java/src/main/java/kata/Cell.java
+++ java/src/main/java/kata/Cell.java
@@ -9,13 +9,24 @@ import java.util.List;
 public class Cell {
 
     private final ArrayList<Cell> cells;
+    private boolean isAlive;
 
     public Cell() {
+        cells = new ArrayList<Cell>();
+        isAlive = true;
+    }
+
+    public Cell(boolean isAlive) {
+        this.isAlive = isAlive;
         cells = new ArrayList<Cell>();
     }
 
     public List<Cell> getNeighbours() {
         cells.add(new Cell());
         return this.cells;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
     }
 }
--- java/src/test/java/kata/KataTest.java
+++ java/src/test/java/kata/KataTest.java
@@ -2,6 +2,7 @@ package kata;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.fest.assertions.Assertions.assertThat;
@@ -17,11 +18,30 @@ public class KataTest {
     }
 
     @Test
+    public void a_cell_can_be_alive() {
+        Cell cell = new Cell(true);
+        assertThat(cell.isAlive()).isTrue();
+    }
+    @Test
+    public void a_cell_can_be_dead() {
+        Cell cell = new Cell(false);
+        assertThat(cell.isAlive()).isFalse();
+    }
+
+    @Test
     public void a_cell_has_live_neighbours() {
         Cell aCell = new Cell();
         List<Cell> neighbors = aCell.getNeighbours();
+        List<Cell> liveNeighbors = new ArrayList<Cell>();
 
-        assertThat(neighbors).isNotEmpty();
+        for (Cell neighbor : neighbors) {
+            if (neighbor.isAlive()) {
+                liveNeighbors.add(neighbor);
+            }
+        }
+
+
+        assertThat(liveNeighbors).isNotEmpty();
 
     }
 
